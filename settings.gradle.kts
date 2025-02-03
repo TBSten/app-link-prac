6575d5c704e38423460103cd456003c3ebbc0b9f
TBSten <programmingCafeteria@gmail.com>
2025-02-03 17:35:00 +0900

    プロジェクト名の修正

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ dependencyResolutionManagement {
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "AndroidProjectTemplate"
+rootProject.name = "AppLinkPrac"
 include(":common")
 include(":common:testing")
 include(":tools:debug")
